bf56e978282bab22c03461ce5e9d31100d5f79db
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2023-11-27 21:48:24 -0300

    Integration Register company Front end -> Back end

--- ergoease_dashboard/src/pages/RegisterPage/RegisterPage.jsx
+++ ergoease_dashboard/src/pages/RegisterPage/RegisterPage.jsx
@@ -2,13 +2,29 @@ import { useEffect } from 'react';
 import  ContainerBackground  from '../../components/ContainerBackground/ContainerBackground';
 import useButtonClick from '../../hooks/useButtonClick';
 import './RegisterPage.scss';
+import axios from 'axios';
+
 
 const RegisterPage = () => {
     const { buttonClickData, handleButtonClick } = useButtonClick();
 
-    useEffect(() => {
-        console.log('data = ', buttonClickData);
-    }, []);
+    useEffect( () => {
+        try {
+            axios.post('http://localhost:3333/cadastroEmpresa', buttonClickData)
+                .then(function (res) {
+
+                console.log(res.data.response)
+
+                })
+                .catch(function (error) {
+                console.error(error)
+                return error
+                });
+        } catch(error) {
+            console.log('error = ', error)
+        }
+    }, [buttonClickData])
+
 
     return ( 
         <div className="content-register">
