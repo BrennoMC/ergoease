31bbe7142ff5a8150ca9486f1f8d882aeafb4ff5
Paulo Henrique Meneghini <paulo.meneghini@outlook.com>
2023-11-27 23:02:48 -0300

    feat/Integration Login company

--- ergoease_dashboard/src/components/ContentLogin/ContentLogin.jsx
+++ ergoease_dashboard/src/components/ContentLogin/ContentLogin.jsx
@@ -3,14 +3,24 @@ import { Link } from 'react-router-dom';
 import ButtonAdvanced from '../ButtonAdvanced/ButtonAdvanced';
 import './ContentLogin.scss';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const ContentLogin = ({
     title,
     forgotPasswordText,
     linkForgotPassword,
+    handleClick,
 }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [login, setLogin] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { handleSubmit } = useForm();
+
+    const formSubmit = () => {
+        handleClick(login)
+    }
 
     return (
         <div className="content-login">
@@ -18,19 +28,19 @@ const ContentLogin = ({
                 <h1>{title}</h1>
             )}
 
-            <form className="content-login__form-login">
+            <form onSubmit={handleSubmit(formSubmit)} className="content-login__form-login">
                 <div className="content-login__form-login__form-label">
                     <label>E-mail</label>
                     <input 
                         type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}    
+                        value={login.email}
+                        onChange={(e) => setLogin({ ...login, email: e.target.value })}    
                     />
                     <label>Senha</label>
                     <input 
                         type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}    
+                        value={login.password}
+                        onChange={(e) => setLogin({ ...login, password: e.target.value })}    
                     />
 
                     <div className="content-login__redirect">
@@ -38,7 +48,7 @@ const ContentLogin = ({
                             {forgotPasswordText}
                         </Link>
                     </div>
-                    <ButtonAdvanced title="Entrar" />
+                    <ButtonAdvanced title="Entrar" type="submit" />
                 </div>
 
             </form>
